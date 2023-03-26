10b12707356e75298632468288a5a7b81432dad2
Stanislav-Zhuk <steys.ua@gmail.com>
2023-03-26 14:49:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,25 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+// get access to elements of DOM
+const countrySearchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// add event listener
+countrySearchBox.addEventListener('input', debounce(handlerSearchOfCountry, DEBOUNCE_DELAY));
+
+// function to handler search of the entered country
+function handlerSearchOfCountry() {
+  fetchCountries(name).then(country => {
+    // logic of country search
+    if (country.length === 1) {
+      countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo(country));
+    } else if (country.length >= 10) {
+      ifTooManyMatchesAlert();
+    } else {
+      countryList.insertAdjacentHTML('beforeend', markupCountryList(country));
+    }
+  }).catch()
+}
